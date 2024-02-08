6037ccadab5cea5c2e8fa6eb1d5010cffc1f0fea
damian.figinski <figo96it@gmail.com>
2024-02-08 20:35:34 +0100

    08.02.24 - add function swap

--- src/main/java/org/example/generictypes/Main.java
+++ src/main/java/org/example/generictypes/Main.java
@@ -10,5 +10,32 @@ public class Main {
 
         System.out.println(first);
         System.out.println(second);
+
+        Integer[] array1= {1,2,3,4,5,6,7,8};
+        array1 = swap(array1, 2, 4);
+        System.out.println(array1[2]+" "+array1[4]);
+    }
+
+    /**
+     * Swaps elements at specified positions in a generic array.
+     *
+     * @param <Number>      The generic type for the array elements.
+     * @param array         The array of elements to perform the swap on.
+     * @param firstIndex    The index of the first element to be swapped.
+     * @param secondIndex   The index of the second element to be swapped.
+     * @return              The array with elements swapped at the specified positions.
+     *                      Returns the original array if indices are invalid or equal.
+     */
+    public static <Number> Number[] swap(Number[] array, int firstIndex, int secondIndex) {
+        // Check if indices are valid and distinct
+        if (firstIndex > array.length - 1 || firstIndex < 0 || secondIndex < 0 || secondIndex > array.length - 1 || firstIndex == secondIndex) {
+            return array;
+        }
+        // Perform the swap
+        Number tmp = array[secondIndex];
+        array[secondIndex] = array[firstIndex];
+        array[firstIndex] = tmp;
+
+        return array; // Return the array with elements swapped
     }
 }
--- src/main/java/org/example/generictypes/Pair.java
+++ src/main/java/org/example/generictypes/Pair.java
@@ -1,19 +1,41 @@
 package org.example.generictypes;
 
+/**
+ * A class representing a pair of objects with generic data types.
+ *
+ * @param <F> Type of the first element of the pair.
+ * @param <S> Type of the second element of the pair.
+ */
 public class Pair<F, S> {
 
-    private F first;
-    private S second;
+    private F first;    // The first element of the pair.
+    private S second;   // The second element of the pair.
 
+    /**
+     * Constructor creating a pair with the given first and second elements.
+     *
+     * @param first  The first element of the pair.
+     * @param second The second element of the pair.
+     */
     public Pair(F first, S second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * Method returning the first element of the pair.
+     *
+     * @return The first element of the pair.
+     */
     public F first() {
         return first;
     }
 
+    /**
+     * Method returning the second element of the pair.
+     *
+     * @return The second element of the pair.
+     */
     public S second() {
         return second;
     }
